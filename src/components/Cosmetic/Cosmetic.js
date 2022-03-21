02bf9db911bfbae4700e9093a9d9188dbf7ad892
amr303 <amr.abdulmatin@gmail.com>
2022-03-22 04:38:19 +0600

    local storage to store data

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import './Cosmetic.css';
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
 
     const addToCart = (id) => {
-        console.log('item added', id);
-
+        addToDb(id);
+        
     }
-    const addToCartWithParam = () => addToCart(id);
+    // const addToCartWithParam = () => addToCart(id);
 
     return (
         <div className='product'>
